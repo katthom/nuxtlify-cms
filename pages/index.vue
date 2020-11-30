86ade399080f7756a387fff492572bb53b9f2f05
katthom <kat.thompson@comcast.net>
2020-11-30 16:55:30 -0600

    some more Identity setup

--- pages/index.vue
+++ pages/index.vue
@@ -24,7 +24,21 @@
         </a>
       </div>
     </div>
+
+    <script>
+      if (window.netlifyIdentity) {
+        window.netlifyIdentity.on("init", user => {
+          if (!user) {
+            window.netlifyIdentity.on("login", () => {
+              document.location.href = "/admin/";
+            });
+          }
+        });
+      }
+    </script>
   </div>
+
+  
 </template>
 
 <script>
