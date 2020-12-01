4de866933ee3cbc97e498ee9292046f23fd4286d
katthom <kat.thompson@comcast.net>
2020-12-01 14:15:04 -0600

    trying to fix blog page refresh to not found problem

--- nuxt.config.js
+++ nuxt.config.js
@@ -52,5 +52,18 @@ export default {
       productionTip: false,
       devtools: true
     }
-  }
+  }, 
+
+  generate: {
+    routes: function() {
+      const fs = require('fs');
+      const path = require('path');
+      return fs.readdirSync('./content/blog').map(file => {
+        return {
+          route: `/blog/${path.parse(file).name}`, // Return the slug
+          payload: require(`./content/blog/${file}`),
+        };
+      });
+    },
+  },
 }
