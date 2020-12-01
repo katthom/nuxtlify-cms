ca06fcf93fad507ec2a536b8e6e76c0e679242f2
katthom <kat.thompson@comcast.net>
2020-12-01 17:02:35 -0600

    add generate in nuxt.config

--- nuxt.config.js
+++ nuxt.config.js
@@ -54,5 +54,16 @@ export default {
     }
   }, 
 
-  
+  generate: {
+    routes: function() {
+      const fs = require('fs');
+      const path = require('path');
+      return fs.readdirSync('./content/blog').map(file => {
+        return {
+          route: `/blog/${path.parse(file).name}`, // Return the slug
+          payload: require(`./content/blog/${file}`),
+        };
+      });
+    },
+  },
 }
