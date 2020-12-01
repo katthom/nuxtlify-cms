1dd6cae93309d780d262260c7ddbb0b3be4b3909
katthom <kat.thompson@comcast.net>
2020-12-01 11:12:22 -0600

    setting up blogs still

--- nuxt.config.js
+++ nuxt.config.js
@@ -45,5 +45,18 @@ export default {
   build: {
   },
 
-  target: 'static'
+  target: 'static',
+
+  generate: {
+    routes: function() {
+      const fs = require('fs');
+      const path = require('path');
+      return fs.readdirSync('./content/blog').map(file => {
+        return {
+          route: `/blog/${path.parse(file).name}`, // Return the slug
+          payload: require(`./content/blog/${file}`),
+        };
+      });
+    },
+  }
 }
