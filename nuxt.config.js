0f528f04272299824ad17a5e1f54112f4bf92889
katthom <kat.thompson@comcast.net>
2020-12-02 15:03:22 -0600

    add generate routes to nuxt.config

--- nuxt.config.js
+++ nuxt.config.js
@@ -54,6 +54,17 @@ export default {
     }
   }, 
 
-  
+  generate: {
+    routes: function() {
+      const fs = require('fs');
+      const path = require('path');
+      return fs.readdirSync('./content/blog').map(file => {
+        return {
+          route: `/blog/${path.parse(file).name}`, //return the slug
+          payload: require(`./content/blog/${file}`),
+        };
+      });
+    },
+  },
   
 }
