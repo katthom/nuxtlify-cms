43228e320ed9935554ab89c49b5be04b468da887
katthom <kat.thompson@comcast.net>
2020-12-09 12:41:33 -0600

    changed blog to articles

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -9,7 +9,7 @@
 <script>
 export default {
    async asyncData({ $content }) {
-    const posts = await $content("blog").fetch();
+    const posts = await $content("articles").fetch();
 
     return {posts};
 
